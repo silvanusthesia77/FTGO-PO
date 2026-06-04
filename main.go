4bc63ee95fd08cf2cbc0677c5e85d4479ab94264
silvanusthesia77 <silvanusthesia1@gmail.com>
2026-06-05 02:30:05 +0700

    Done

--- main.go
+++ main.go
@@ -7,4 +7,22 @@ func main() {
 	age := 23
 
 	fmt.Printf("Nama %s , Usia %d \n", name, age)
+
+	// Multiple variable declarations
+	name1, name2, name3 := "wanus", "Lukas", "Marvel"
+	var first, second, third int = 1, 44, 55
+	devision, rank, hobby := "wanus", 22, "Sport"
+	fmt.Println(name1, name2, name3)
+	fmt.Println(first, second, third)
+	fmt.Println(devision, rank, hobby)
+
+	// Underscore variable
+	var firstVariable string
+
+	fruit, drink, seafood := "manggo", "juice", "fish"
+
+	_, _, _, _ = firstVariable, fruit, drink, seafood
+	fmt.Println(seafood, fruit)
 }
+
+// Data type
