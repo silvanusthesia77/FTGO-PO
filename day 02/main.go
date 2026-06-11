0232d3aea4bed26d8264a903205d3a23138bc99e
Silvanus <silvanusthesia1@gmail.com>
2026-06-11 14:48:21 +0700

    Done

--- day 02/main.go
+++ day 02/main.go
@@ -3,15 +3,62 @@ package main
 import "fmt"
 
 func main() {
-	learnCondition()
+	// learnCondition()
+	learnSwitch()
 }
 
 func learnCondition() {
 	currentYear := 2021
 
-	if age := currentYear - 2012; age < 17 {
+	if age := currentYear - 2019; age < 17 {
 		fmt.Println("Belum Dapat Cetak KTP")
 		return
 	}
 	fmt.Println("Dapat Mencetak KTP")
 }
+func learnSwitch(){
+	// score := 6
+
+	// switch score {
+	// case 8:
+	// 	fmt.Println("Perfect")
+	// case 7:
+	// 	fmt.Println("awesome")
+	// default:
+	// 	fmt.Printf("not bad")
+	// }
+
+	fmt.Println("Case yang lain:")
+	// hasi := 8
+
+	// switch  {
+	// case hasi == 8:
+	// 	fmt.Println("Done")
+	// case (hasi < 9) && (hasi > 4):
+	// 	fmt .Println("Lumayan")
+	// default:
+	// 	{
+	// 		fmt.Println("Study harder")
+	// 		fmt.Println("you need to learn")
+	// 	}
+		
+	// }
+
+		// fallthrouhg
+	hasil := 9
+	switch  {
+	case (hasil <= 10) && (hasil >= 8):
+		fmt.Println("Great Job")
+		fallthrough
+	case (hasil <= 7) && (hasil >= 6):
+		fmt.Println("Good")
+	default:
+		{
+			fmt.Println("Study Hard")
+			fmt.Println("You Don't Have a Good Score")
+		}
+		
+	}
+}
+
+// Nested Conditions
