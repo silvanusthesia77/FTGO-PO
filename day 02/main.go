247d399638b68a1248067ff07b6569a9bca381ae
Silvanus <silvanusthesia1@gmail.com>
2026-06-15 18:17:32 +0700

    Done

--- day 02/main.go
+++ day 02/main.go
@@ -4,7 +4,10 @@ import "fmt"
 
 func main() {
 	// learnCondition()
-	learnSwitch()
+	// learnSwitch()
+	// learnNested()
+	learnLooping()
+	// latihan()
 }
 
 func learnCondition() {
@@ -62,3 +65,62 @@ func learnSwitch(){
 }
 
 // Nested Conditions
+
+func learnNested(){
+	nilai := 0
+
+	if nilai > 7 {
+		switch nilai{
+		case 8:
+			fmt.Println("Perfect")
+		default:
+			fmt.Println("Nice")
+		}
+	}else{
+		if nilai == 5 {
+		fmt.Println("Not Bad")	
+		}else if nilai == 3  {
+			fmt.Println("Keep Trying")
+		}else{
+			fmt.Println("You Can Do It")
+			if nilai == 0 {
+				fmt.Println("try Harder")
+			}
+		}
+
+	}
+}
+ func latihan(){
+	angka := 22
+
+	if like:= angka - 21; like <5 {
+		fmt.Println("Sealmat Datang")
+	}else{
+		fmt.Println("Lain waktu")
+	}
+ }
+
+ func learnLooping(){
+	for i := 0; i < 4; i++ {
+		fmt.Println("Angka", i)
+	}
+
+	j := 0
+
+	for j < 5{
+		fmt.Println("Hasil AKhir :",j)
+		j++
+	}
+
+	k := 0
+
+	for{
+		fmt.Println("Halo :",k)
+		k++
+
+		if k == 5{
+		break
+		}
+	}
+ }
+// 15Loopings (third way of looping)
